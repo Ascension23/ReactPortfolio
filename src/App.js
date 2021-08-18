4ec852dbec50b2f678b2bf2e1781776866829389
Adrian Aguilar <Tygor23@gmail.com>
2021-08-18 10:50:26 -0700

    adding components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import NavMenu from './components/Navbar';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 import './index.css'
 
 function App() {
   return (
-    <>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <NavMenu />
-    </>
+      <About />
+      <Portfolio />
+      <Skills />
+      <Contact />
+    </main>
   );
 }
 
