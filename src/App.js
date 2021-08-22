01c45d7e9242c4c091deaad8390e6085e4913dc5
Adrian Aguilar <Tygor23@gmail.com>
2021-08-22 12:31:55 -0700

    testing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import NavMenu from './components/Navbar';
+import React, { Component } from 'react';
+import Header from './components/Header';
 import About from './components/About';
+import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import './index.css'
-
-function App() {
-  return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <NavMenu />
-      <About />
-      <Portfolio />
-      <Skills />
-      <Contact />
-    </main>
-  );
+import Testimonials from './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+render() {
+return (
+<div className='App'>
+<Header resumeData={resumeData}/>
+<About resumeData={resumeData}/>
+<Resume resumeData={resumeData}/>
+<Portfolio resumeData={resumeData}/>
+<Testimonials resumeData={resumeData}/>
+<ContactUs resumeData={resumeData}/>
+<Footer resumeData={resumeData}/>
+</div>
+);
 }
-
+}
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    'imagebaseurl':'https://github.com/Ascension23',
+    'name': 'Adrian Aguilar',
+    'role': 'Full Stack Developer',
+    'linkedinId':'Ascension23',
+    'skypeid': 'Your skypeid',
+    'roleDescription': " I am eager to learn and am constantly trying to expand my skills. When I'm not coding, I'm reading, spending time with my family or playing video games. ",
+    'socialLinks':[
+    {
+    'name':'linkedin',
+    'url':'https://www.linkedin.com/in/ascension23/',
+    'className':'fa fa-linkedin'
+    },
+    {
+    'name':'github',
+    'url':'http://github.com/Ascension23',
+    'className':'fa fa-github'
+    },
+    ],
+    'aboutme':"My name is Adrian Aguilar, and I am a Full Stack Developer. I live in Sacramento, CA and I'm currently enrolled in the UC Davis Full Stack Development program where I am honing my skills every day and adding new ones to my repertoire. My goal is to take the skills learned in my current program into a career and become a valued member of a team. My past jobs have given me the skills to work in a team environment as well as working alone when tackling tasks. I am eager to find my place in a company in need of my skills.",
+    'address':'Sacramento, CA',
+    'website':'https://github.com/Ascension23',
+    'education':[
+    {
+    'UniversityName':'UC Davis',
+    'specialization':'Cyber Security Bootcamp',
+    'MonthOfPassing':'September',
+    'YearOfPassing':'2021',
+    // 'Achievements':'Some Achievements'
+    },
+    {
+    'UniversityName':'Tracy High School',
+    'specialization':'High School Diploma',
+    'MonthOfPassing':'June',
+    'YearOfPassing':'2000',
+    // 'Achievements':'Some Achievements'
+    }
+    ],
+    'work':[
+    {
+    'CompanyName':'Earl Legette Elementary School',
+    'specialization':'Yard Aide',
+    'MonthOfLeaving':'October',
+    'YearOfLeaving':'2019 - Current',
+    // 'Achievements':'Some Achievements'
+    },
+    {
+    'CompanyName':'FedEx Express',
+    'specialization':'Delivery Driver',
+    'MonthOfLeaving':'October 2012 -',
+    'YearOfLeaving':'November 2015',
+    // 'Achievements':'Some Achievements'
+    }
+    ],
+    'skillsDescription':'Your skills here',
+    'skills':[
+    {
+    'skillname':'HTML5'
+    },
+    {
+    'skillname':'CSS'
+    },
+    {
+    'skillname':'Reactjs'
+    },
+    {
+    'skillname':'JavaScript'
+    }
+    ],
+    'portfolio':[
+    {
+    'name':'Poke Pound',
+    'description':'App',
+    'imgurl':'images/portfolio/PokePound.png',
+    'link': 'https://pokemonstore1.herokuapp.com/'
+    },
+    {
+    'name':'Daily Vibes',
+    'description':'App',
+    'imgurl':'images/portfolio/DailyVibes.png'
+    },
+    {
+    'name':'Weather Dashboard',
+    'description':'App',
+    'imgurl':'images/portfolio/WeatherDashboard.png'
+    },
+    {
+    'name':'Scheduler',
+    'description':'App',
+    'imgurl':'images/portfolio/Scheduler.png'
+    }
+    ],
+    'testimonials':[
+    {
+    'description':'This is a sample testimonial',
+    'name':'Some technical guy'
+    },
+    {
+    'description':'This is a sample testimonial',
+    'name':'Some technical guy'
+    }
+    ]
+    }
+    export default resumeData
